8dd99267f0a45c750c484618cb2f862c4fde8360
Rafael Carneiro de Moraes <rafinhacarneiro@users.noreply.github.com>
2018-07-11 23:34:49 -0300

    Update links-home.php

--- hooks/links-home.php
+++ hooks/links-home.php
@@ -30,19 +30,19 @@
     );
 
     $homeLinks[] = array(
-        'url' => 'http://organizer-test.primecontrol.com.br/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta', 
+        'url' => './tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta', 
         'title' => 'Vagas Abertas', 
         'description' => 'Mostrar vagas abertas de:<br>'.
         
-                         '<a href="http://organizer-test.primecontrol.com.br/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Est%C3%A9fane+Braga&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Estéfane Braga<a>&nbsp;| '. // Estéfane
+                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Est%C3%A9fane+Braga&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Estéfane Braga<a>&nbsp;| '. // Estéfane
         
-                         '<a href="http://organizer-test.primecontrol.com.br/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Fernanda+Lessa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Fernanda Lessa<a>&nbsp;| '. // Fernanda
+                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Fernanda+Lessa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Fernanda Lessa<a>&nbsp;| '. // Fernanda
                          
-                         '<a href="http://organizer-test.primecontrol.com.br/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Hadelen+Lima&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Hadelen Lima<a>&nbsp;| '. // Hadelen
+                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Hadelen+Lima&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Hadelen Lima<a>&nbsp;| '. // Hadelen
                          
-                         '<a href="http://organizer-test.primecontrol.com.br/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Rafael+Silva&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Rafael Silva<a>&nbsp;| '. // Rafael
+                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Rafael+Silva&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Rafael Silva<a>&nbsp;| '. // Rafael
         
-                         '<a href="http://localhost/prime_organizer/tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Raquel+Costa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Raquel Costa<a>', // Raquel
+                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Raquel+Costa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Raquel Costa<a>', // Raquel
         'groups' => array('Admins','RH'), // Mudar para ('RH','Comercial','Financeiro', 'Admins') no servidor
         'grid_column_classes' => 'col-sm-6 col-md-4 col-lg-3', // optional CSS classes to apply to link block. See: http://getbootstrap.com/css/#grid
         'panel_classes' => 'panel panel-info', // optional CSS classes to apply to panel. See: http://getbootstrap.com/components/#panels

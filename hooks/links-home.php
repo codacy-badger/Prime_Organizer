77b437948ba32f631e3668e1569f1f4b18e07b6a
Rafael Carneiro de Moraes <rafinhacarneiro@users.noreply.github.com>
2018-07-16 10:05:26 -0300

    Recrutadores são adicionados automáticamente no link da home

--- hooks/links-home.php
+++ hooks/links-home.php
@@ -28,22 +28,19 @@
         'icon' => '', // optional icon to use with the link
         'table_group' => '' // optional name of the table group you wish to add the link to. If the table group name contains non-Latin characters, you should convert them to html entities.
     );
-
+    
     // Link para os filtros de vagas
+    $query = sql("SELECT str_nome FROM tb_recrutador", $eo);
+    $res = db_fetch_assoc($query);
+    
+    for($i = 0; $i < count($res); $i++){
+        $recrutadores = $recrutadores . '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D='. urlencode($res[$i]) .'&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">'. $res[$i] .'<a>&nbsp;| ';
+    }
+
     $homeLinks[] = array(
         'url' => './tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta', 
         'title' => 'Vagas Abertas', 
-        'description' => 'Mostrar vagas abertas de:<br>'.
-        
-                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Est%C3%A9fane+Braga&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Estéfane Braga<a>&nbsp;| '. // Estéfane
-        
-                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Fernanda+Lessa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Fernanda Lessa<a>&nbsp;| '. // Fernanda
-                         
-                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Hadelen+Lima&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Hadelen Lima<a>&nbsp;| '. // Hadelen
-                         
-                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Rafael+Silva&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Rafael Silva<a>&nbsp;| '. // Rafael
-        
-                         '<a href="./tb_vaga_view.php?SortField=&SortDirection=&FilterAnd%5B1%5D=and&FilterField%5B1%5D=10&FilterOperator%5B1%5D=equal-to&FilterValue%5B1%5D=Raquel+Costa&FilterAnd%5B2%5D=and&FilterField%5B2%5D=12&FilterOperator%5B2%5D=equal-to&FilterValue%5B2%5D=Aberta">Raquel Costa<a>', // Raquel
+        'description' => 'Mostrar vagas abertas de:<br>'. $recrutadores,
         'groups' => array('Admins','RH'), // Mudar para ('RH','Comercial','Financeiro', 'Admins') no servidor
         'grid_column_classes' => 'col-sm-6 col-md-4 col-lg-3', // optional CSS classes to apply to link block. See: http://getbootstrap.com/css/#grid
         'panel_classes' => 'panel panel-info', // optional CSS classes to apply to panel. See: http://getbootstrap.com/components/#panels

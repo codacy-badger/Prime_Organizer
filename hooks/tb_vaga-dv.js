28e09e263b51d389de374ceb1ebb86e2db028975
Rafael Carneiro de Moraes <rafinhacarneiro@users.noreply.github.com>
2018-07-27 11:52:00 -0300

    Previne edição da vaga caso esteja encerrada ou usuário não tenha permissão

--- hooks/tb_vaga-dv.js
+++ hooks/tb_vaga-dv.js
@@ -1,3 +1,21 @@
+// Recupera o cookie com a groupID do usuário
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
 $j(function(){
 
     // Esconde os campos Detalhe de Status, Data de Inicio e Canal Fechamento se a vaga foi encerrada
@@ -8,7 +26,6 @@ $j(function(){
         if(Status == 'Encerrada'){
             // Mostra o campo Detalhe do Status
             $j('#str_detalhe_status').parents('.form-group').show();
-
         // Senão
         } else{
             // Mostra os campos Data de inicio e Observação
@@ -16,6 +33,23 @@ $j(function(){
             $j('#str_detalhe_status, #dta_inicio, #canal_fechamento').parents('.form-group').hide();
         }
 
+    }).change();
+    
+    
+    // Desativa os campos se a vaga está encerrada
+    $j('#dta_previsao_fechamento, #recrutador_id, #str_prioridade').on('change', function(){
+        var Status = $j('#str_status').val();
+        
+        if(Status == 'Encerrada'){
+            $j(this).prop({
+                disabled: true        
+            });
+        } else{
+            $j(this).prop({
+                disabled: false        
+            });
+        }
+        
     }).change();
 
     
@@ -51,16 +85,24 @@ $j(function(){
         }
     });
     
-    // Esconde os botões Salvar/Inserir se a vaga está encerrada
+    // Esconde os botões Salvar/Inserir/Editar se a vaga está encerrada ou usuário não possui permissão
     $j('#update, #insert, #delete, #editar').on('change', function(){
         
-        var Status = $j('#str_status').val();
+        var groupID = parseInt(getCookie('groupID'));
+        
+        if(groupID == 2 || groupID == 3 || groupID == 8){
+        
+            var Status = $j('#str_status').val();
 
-        if(Status == "Encerrada"){
-            $j('#update, #insert, #editar').parents('.btn-group-vertical').hide();
-            $j('#delete').hide();
-        } else {
-            $j(this).parents('.btn-group-vertical').show();
+            if(Status == "Encerrada"){
+                $j('#update, #insert, #editar').parents('.btn-group-vertical').hide();
+                $j('#delete').hide();
+            } else {
+                $j(this).parents('.btn-group-vertical').show();
+            }
+            
+        } else{
+            $j(this).parents('.btn-group-vertical').hide();
         }
         
     }).change();
